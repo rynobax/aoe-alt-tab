4c857eabce01c9f2d9af582309e8fba526c37243
Ryan Baxley <ryan.baxley1@gmail.com>
2020-05-20 16:42:55 -0400

    Convert to typescript

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,10 +6,9 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./Header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -42,10 +41,6 @@ const Layout = ({ children }) => {
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
