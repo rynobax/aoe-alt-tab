d052121889ecf6e143ead9d16173321fc29afdcb
Ryan Baxley <ryan.baxley1@gmail.com>
2020-05-21 21:39:30 -0400

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-102175338-3",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
